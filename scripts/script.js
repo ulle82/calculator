4bde242f1650ddb574b9c842b6a20a5a3e2b3806
Ulrik Wichmann <28593903+ulle82@users.noreply.github.com>
2026-05-13 23:08:16 +0200

    Finished basic HTML and CSS for calculator

---
+++ scripts/script.js
@@ -0,0 +1,28 @@
+let output = 223
+let numberOne = ""
+let numnberTwo = ""
+let operator = ""
+
+const display = document.querySelector('#display');
+
+function updateDisplay() {
+    display.textContent = (output).toLocaleString();
+}
+
+updateDisplay();
+
+function add() {
+
+}
+
+function subtract() {
+    
+}
+
+function multiply() {
+
+}
+
+function divide() {
+
+}
